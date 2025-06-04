b2cdd4a63641cb5f036cbfae1a78a96b9c127003
jefte-josh <92620774+jefte-josh@users.noreply.github.com>
2025-06-05 00:52:23 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['localhost'],
-    unoptimized: true,
-  },
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/media-gallery' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/media-gallery/' : '',
-}
+  basePath: '/media-gallery',
+  assetPrefix: '/media-gallery/',
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
